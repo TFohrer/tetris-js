ad88f24f5b9f100d8a572f72fc344044b5661844
Tarek Fohrer <tarek.fohrer@umwerk.com>
2020-02-29 23:37:41 +0100

    first version

---
+++ constants.js
@@ -0,0 +1,13 @@
+"use strict";
+
+const ROWS = 20;
+const COLS = 10;
+const BLOCK_SIZE = 30;
+
+const KEY = {
+    LEFT: 37,
+    RIGHT: 39,
+    DOWN: 40
+};
+// make it immutable
+Object.freeze(KEY);
---
+++ piece.js
@@ -0,0 +1,45 @@
+class Piece {
+    x;
+    y;
+    color;
+    shape;
+    ctx;
+
+    constructor(ctx) {
+        this.ctx = ctx;
+        this.spawn();
+    }
+
+    spawn() {
+        this.color = 'blue';
+        this.shape = [
+            [2, 0, 0],
+            [2, 2, 2],
+            [0, 0, 0]
+        ];
+
+        // Starting position.
+        this.x = 3;
+        this.y = 0;
+    }
+
+    // we loop through all the cells of the shape if the value in the cell is greater than zero, then we color that block
+    draw() {
+        this.ctx.fillStyle = this.color;
+        this.shape.forEach((row, y) => {
+            row.forEach((value, x) => {
+                // this.x, this.y gives the left upper position of the shape
+                // x, y gives the position of the block in the shape
+                // this.x + x is then the position of the block on the board
+                if (value > 0) {
+                    this.ctx.fillRect(this.x + x, this.y + y, 1, 1);
+                }
+            });
+        });
+    }
+
+    move(p) {
+        this.x = p.x;
+        this.y = p.y;
+    }
+}
